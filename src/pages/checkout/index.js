8f98fe6fdc2d2c914aa54a49e384f78be9de81bd
malika <malika.derfoufii@gmail.com>
2022-03-02 00:26:15 +0100

    fix bugg

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import styles from './checkout.module.scss';
-const handleConfirmation = async () => {
-    const payload = {
-      //plan: plan.label,
-      total: plan.price,
-    };
+import Link from 'next/react';
+import Link from "next/link";
 
-    try {
-      setStep(step + 1);
-      const stripe = await stripePromise;
-      const response = await stripeService.createSession(payload, plan);
+// const handleConfirmation = async () => {
+//     const payload = {
+//       //plan: plan.label,
+//       total: plan.price,
+//     };
 
-      await authService.signup(account).then((data) => {
-        localStorage.setItem("token", JSON.stringify(data.token));
-        console.log(data);
-      })
+//     try {
+//       setStep(step + 1);
+//       const stripe = await stripePromise;
+//       const response = await stripeService.createSession(payload, plan);
 
-      await stripe.redirectToCheckout({
-        sessionId: response.id,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//       await authService.signup(account).then((data) => {
+//         localStorage.setItem("token", JSON.stringify(data.token));
+//         console.log(data);
+//       })
+
+//       await stripe.redirectToCheckout({
+//         sessionId: response.id,
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
 const Index = () => {
     return (
@@ -34,14 +37,14 @@ const Index = () => {
             <div className={styles.Third__Container}>
             <ul>
            <li>
-               <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="checkmark-group--icon" aria-hidden="true">
-               <path fill-rule="evenodd" clip-rule="evenodd" d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
+               <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  aria-hidden="true">
+               <path fill-rule="evenodd"  d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
            <span>Recommandations personnalisées.</span>
            </li> 
-           <li><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="checkmark-group--icon" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
+           <li><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  aria-hidden="true"><path   d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
            <span>Changez ou annulez votre forfait à tout moment.</span>
            </li> 
-           <li><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="checkmark-group--icon" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
+           <li><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  aria-hidden="true"><path   d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
            <span>Accès illimité sur tous vos appareils.</span>
            </li> 
             </ul>
@@ -51,18 +54,24 @@ const Index = () => {
                    <label>
                        {/* <input type="radio" name="plan"/> */}
                        <span >
-                          <a className={styles.plan} href='/browse' onClick={handleConfirmation} >
+                          <Link href='/browse' > <a className={styles.plan} 
+                        //   onClick={handleConfirmation} 
+                          >
                            Abonnement normale
                            </a>
+                           </Link>
                         </span>
                    </label>
                    <label>
                        {/* <input type="radio" name="plan" /> */}
 
                        <span>
-                            <a  className={styles.plan} href='/browse'  onClick={handleConfirmation} >
+                       <Link href='/browse' ><a  className={styles.plan} href='/browse' 
+                        // onClick={handleConfirmation} 
+                        >
                                 Abonnement premium                   
                             </a>
+                       </Link>
                         </span>
                    </label>
                    {/* <label>
