7f4fa28bda4f268191effeca1446bbd4473deec6
malika <malika.derfoufii@gmail.com>
2022-03-02 00:53:48 +0100

    fix bug 3

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -30,12 +30,12 @@ const Index = () => {
     return (
         <div className={styles.First__Container}>
             <div className={styles.Second__Container}>
-            <h3>ÉTAPE <span>2 </span>SUR <span>3</span></h3>
+            <h3>ÉTAPE <span>2 </span>SUR <span>2</span></h3>
             <h1>Regardez autant que vous voulez. Sans publicité.</h1>
             </div>
             <div className={styles.Third__Container}>
-            <ul>
-           <li>
+             {/* <ul>
+          <li>
                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  aria-hidden="true">
                <path fill-rule="evenodd"  d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
            <span>Recommandations personnalisées.</span>
@@ -44,10 +44,15 @@ const Index = () => {
            <span>Changez ou annulez votre forfait à tout moment.</span>
            </li> 
            <li><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  aria-hidden="true"><path   d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z" fill="#e50914"></path></svg>
+          
+          
+          
            <span>Accès illimité sur tous vos appareils.</span>
-           </li> 
-            </ul>
+           </li>  
+            </ul>*/}
+           
             </div>
+          
             <div className={styles.plan__Container}>
                 <div className={styles.planGrid__header}>
                    <label>
